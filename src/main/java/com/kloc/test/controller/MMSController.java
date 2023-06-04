801323ce6ca12f06dbabcc2882b80a51041988b7
Avinash2581 <sahuavinash91141@gmail.com>
2023-06-04 22:06:07 +0530

    Third Commit

--- src/main/java/com/kloc/test/controller/MMSController.java
+++ src/main/java/com/kloc/test/controller/MMSController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +28,7 @@ import com.kloc.test.service.DrugService;
 import com.kloc.test.service.InventoryService;
 import com.kloc.test.service.OrderDetailsService;
 import com.kloc.test.service.VendorService;
-
+@CrossOrigin("*")
 @RestController()
 @RequestMapping("/api")
 public class MMSController {
